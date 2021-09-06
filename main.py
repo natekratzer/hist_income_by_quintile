632867cab0275ef73d759421b1e7d82d6082e7d1
natekratzer <nate.kratzer@gmail.com>
2021-09-06 11:44:49 -0400

    change to data source with more history available

--- main.py
+++ main.py
@@ -2,9 +2,9 @@
 import pandas as pd
 
 ## Read in quintile data
-# I made a csv of table H-3 
-# All values are in 2019 dollars as provided by the Census: https://www.census.gov/data/tables/time-series/demo/income-poverty/historical-income-households.html
-hist_income = pd.read_csv("hist_income.csv")
+# I made a csv of table F-1: https://www.census.gov/data/tables/time-series/demo/income-poverty/historical-income-families.html
+# All values are in 2019 dollars as provided by the Census
+hist_income = pd.read_csv("hist_income47.csv")
 
 ## Cleaning
 # Footnotes for the data are available here: https://www.census.gov/topics/income-poverty/income/guidance/cps-historic-footnotes.html
@@ -16,28 +16,14 @@ hist_income['Year'] = hist_income['Year'].str.replace(r"\(.*\)" , "")
 #remove all commas
 hist_income = hist_income.apply(lambda x: x.str.replace(',', '')) #using a lambda function and applying to all columns
 
-# Rename columns
-rename_dict = {
-    "Year" : "year",
-    "Lowest\nfifth" : "0-20",
-    "Second\nfifth" : "20-40",
-    "Middle\nfifth" : "40-60",
-    "Fourth\nfifth" : "60-80",
-    "Highest\nfifth" : "80-100",
-    "Top 5\npercent" : "95-100"
-}
-
-hist_income = hist_income.rename(columns = rename_dict)
-
 # Dictionary to retype columns
 type_dict = {
-    "year"  : "datetime64[ns]", 
-    "0-20"  : "int32",
-    "20-40" : "int32",
-    "40-60" : "int32",
-    "60-80" : "int32",
-    "80-100": "int32",
-    "95-100": "int32"
+    "Year"  : "datetime64[ns]", 
+    "20"  : "int32",
+    "40" : "int32",
+    "60" : "int32",
+    "80" : "int32",
+    "95": "int32"
 }
 
 # Change data types
@@ -45,7 +31,7 @@ hist_income = hist_income.astype(type_dict)
 
 # Average of years with multiple values
 # See linked footnotes above - two slightly different methods available for each year
-hist_income = hist_income.groupby(['year']).mean() #this also turns year into my index, which is convenient for later applying changes to all columns except year
+hist_income = hist_income.groupby(['Year']).mean() #this also turns year into my index, which is convenient for later applying changes to all columns except year
 
 print(hist_income.head())
 
@@ -56,8 +42,10 @@ hist_income = hist_income.apply(lambda x: x * 100) # multiply by 100 for conveni
 print(hist_income.head())
 
 ## Pull in presidential data
+pres = pd.read_csv("pres_year.csv")
 
 # Join datasets
+df = pd.merge(hist_income, pres, )
 
 # Plot data in altair
 
